97881acea8f814f328bbe99a7e263c5378be51f2
Arely Guevara <sofia.guevara@salem.edu>
2020-10-02 02:35:36 -0400

    hotfix: updated mobile view and modal

--- scripts/script.js
+++ scripts/script.js
@@ -2,28 +2,40 @@ let modal = document.querySelector(".modal");
 let modalClose = document.querySelector('.modal__close');
 let profileAdd = document.querySelector('.profile__edit');
 
-let modalSubmit = document.querySelector(".modal__submit"); 
-
-
 let modalForm = document.querySelector('.modal__form');
 
-let nameInput = document.querySelector('.modal__input_name');
-let jobInput = document.querySelector('.modal__input_job');
+let nameInput = document.querySelector('.input_name');
+let jobInput = document.querySelector('.input_job');
 
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+
 function handleForm(e) {
     e.preventDefault();
 
-    modal.classList.toggle("modal_open")
-
+    
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+    closeModal(e);
+}
+
+function openModal(e) {
+    e.preventDefault();
+
+    
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+    modal.style.display = "flex";
+}
+
+function closeModal(e) {
+    e.preventDefault();
+
+    modal.style.display = "none";
 }
 
 modalForm.addEventListener("submit", handleForm);
 
-profileAdd.addEventListener("click", modalToggle);
-modalClose.addEventListener("click", modalToggle)
-
+profileAdd.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal)
